7b79a24f616bf67fbaafcda74aa3ab096832ba47
meloneminze <melanie@derrabus.de>
2020-03-06 16:44:36 +0100

    Add h1 and search field

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import { app } from './app';
 
+//Für jedes Element wird ein Element in Body geschrieben
 const elements = app();
 elements.forEach(element => {
   document.body.appendChild(element);
