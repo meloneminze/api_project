ea8e9d56fdc84e2da7aba7cd046b03fa03606a28
meloneminze <melanie@derrabus.de>
2020-03-07 20:42:31 +0100

    Fixed apiButton

--- src/app.js
+++ src/app.js
@@ -11,75 +11,29 @@ export function app() {
     className: 'main'
   });
 
-  //   const quoteImg = createElement('img', {
-  //     className: 'img'
-  //   });
-
-  //   function imgCreate(src) {
-  //     const img = document.createElement('img');
-  //     image = src;
-  //     return img;
-  //   }
-
-  //   const quoteImg = new Image(100, 100);
-  //   quoteImg.src = fetchData(quote.image);
-  //   document.body.appendChild(quoteImg);
-
-  const titleElement = title('Simpsons Quotes');
-  const showButton = createNewQuoteButton();
-
-  const quote = fetchData();
-  const quoteElement = createElement('div', {
-    className: 'quote'
-  });
-
-  fetchData().then(quote => {
-    quoteElement.appendChild(document.createTextNode(quote.quote));
-    quoteElement.appendChild(document.createTextNode(quote.character));
-    //quoteImg.appendChild(document.createTextNode(quote.image));
-  });
-  const quoteImg = new Image();
-  quoteImg.src = document.createTextNode(quote.image);
-
+  const titleElement = title('Simspons Quotes');
   header.appendChild(titleElement);
-  main.appendChild(quoteElement);
-  main.appendChild(quoteImg);
 
-  main.appendChild(showButton);
+  const section = createElement('div', {});
+  main.appendChild(section);
+
+  const buttonElement = createNewQuoteButton();
+  main.appendChild(buttonElement);
+
+  const loadData = async function() {
+    const response = await fetch(
+      'https://thesimpsonsquoteapi.glitch.me/quotes'
+    );
+    const quote = (await response.json())[0];
+    section.innerHTML = '';
+    section.appendChild(createElement('p', { innerText: quote.quote }));
+    section.appendChild(createElement('p', { innerText: quote.character }));
+    section.appendChild(createElement('img', { src: quote.image }));
+  };
+
+  buttonElement.addEventListener('click', loadData);
+
+  loadData();
 
   return [header, main];
 }
-
-//const button = document.querySelector('button');
-// showButton.addEventListener('click', event => {
-//   fetchData();
-// });
-
-//Api-Funktion, response holt daten
-async function fetchData() {
-  const response = await fetch(
-    'https://thesimpsonsquoteapi.glitch.me/quotes?count=1',
-    //'https://httpbin.org/status/400',
-    { mode: 'cors' }
-  );
-  if (response.status > 200) {
-    throw 'Something went wrong';
-  }
-  const [quote] = await response.json();
-  return quote;
-}
-
-//Erster Versuch
-//showButton.addEventListener(click, fetchData);
-//   async function fetchData() {
-//     const response = await fetch(
-//       'https://thesimpsonsquoteapi.glitch.me/quotes?count=1',
-//       //'https://httpbin.org/status/400',
-//       { mode: 'cors' }
-//     ); //holt Daten
-//     if (response.status > 200) {
-//       throw 'Something went wrong';
-//     }
-//     const [quote] = await response.json();
-//     return quote;
-//   }
